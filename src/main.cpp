dd19e441bc82d48e45f292a496bfe27c12e7a9fe
Serge office <cncserge@gmail.com>
2023-04-21 13:52:11 +0300

    add doc, define GPIO

--- src/main.cpp
+++ src/main.cpp
@@ -12,22 +12,39 @@
 #include <ELECHOUSE_CC1101_SRC_DRV.h>
 #include <RCSwitch.h>
 RCSwitch mySwitch = RCSwitch();
-
+enum {pult_open_isRun, pult_close_isRun};
+const int in_estop_close  = 16;
+const int in_estop_open   = 17;
+const int out_pwm         = 13;
+const int out_relay_close = 27;
+const int out_relay_open  = 26;
 void setup() {
     Serial.begin(9600);
-    if (ELECHOUSE_cc1101.getCC1101()){       // Check the CC1101 Spi connection.
-        Serial.println("Connection OK");
+    { // config PWM
+        /*
+            ledcSetyp
+            ledcAttachPin
+            ledcWrite
+        */
+        ledcSetup(0, 1000, 8);
+        ledcAttachPin(out_pwm, 0);
+        ledcWrite(0, 0);
     }
-    else{
-        Serial.println("Connection Error");
+    {// config cc1101
+        if (ELECHOUSE_cc1101.getCC1101()){       // Check the CC1101 Spi connection.
+            Serial.println("Connection OK");
+        }
+        else{
+            Serial.println("Connection Error");
+        }
+        //CC1101 Settings:                (Settings with "//" are optional!)
+        ELECHOUSE_cc1101.Init();            // must be set to initialize the cc1101!
+        //ELECHOUSE_cc1101.setRxBW(812.50);  // Set the Receive Bandwidth in kHz. Value from 58.03 to 812.50. Default is 812.50 kHz.
+        //ELECHOUSE_cc1101.setPA(10);       // set TxPower. The following settings are possible depending on the frequency band.  (-30  -20  -15  -10  -6    0    5    7    10   11   12)   Default is max!
+        ELECHOUSE_cc1101.setMHZ(433.92); // Here you can set your basic frequency. The lib calculates the frequency automatically (default = 433.92).The cc1101 can: 300-348 MHZ, 387-464MHZ and 779-928MHZ. Read More info from datasheet.
+        mySwitch.enableReceive(2);  // Receiver on interrupt 0 => that is pin #2
+        ELECHOUSE_cc1101.SetRx();  // set Receive on
     }
-    //CC1101 Settings:                (Settings with "//" are optional!)
-    ELECHOUSE_cc1101.Init();            // must be set to initialize the cc1101!
-    //ELECHOUSE_cc1101.setRxBW(812.50);  // Set the Receive Bandwidth in kHz. Value from 58.03 to 812.50. Default is 812.50 kHz.
-    //ELECHOUSE_cc1101.setPA(10);       // set TxPower. The following settings are possible depending on the frequency band.  (-30  -20  -15  -10  -6    0    5    7    10   11   12)   Default is max!
-    ELECHOUSE_cc1101.setMHZ(433.92); // Here you can set your basic frequency. The lib calculates the frequency automatically (default = 433.92).The cc1101 can: 300-348 MHZ, 387-464MHZ and 779-928MHZ. Read More info from datasheet.
-    mySwitch.enableReceive(2);  // Receiver on interrupt 0 => that is pin #2
-    ELECHOUSE_cc1101.SetRx();  // set Receive on
 }
 
 void loop() {
